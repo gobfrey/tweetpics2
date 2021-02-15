ba25280827261d12c64a65ebbd5607b4da788123
Adam Field <adam@adamfield.net>
2021-02-15 12:05:39 -0800

    updater class and updater script

---
+++ lib/TweetPics/Updater.pm
@@ -0,0 +1,31 @@
+package TweetPics::Updater;
+
+use strict;
+use warnings;
+
+
+sub new
+{
+	my ($class, $source, $persistence) = @_;
+
+	return bless {
+		source => $source,
+		persistence => $persistence
+	}, $class;
+}
+
+sub update
+{
+	my ($self) = @_;
+
+	my $source = $self->{source};
+	my $persistence = $self->{persistence};
+
+	while (my $post = $source->next_post())
+	{
+		last if $persistence->post_exists($post); #harvest until you see one you've seen before
+		$persistence->write_post($post);
+	}
+}
+
+1;
