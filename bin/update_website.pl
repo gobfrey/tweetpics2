b4add53584f5621f82da969d5427ac0c3ed218bf
Adam Field <adam@adamfield.net>
2021-03-09 08:23:52 -0800

    slight refactoring

--- bin/update_website.pl
+++ bin/update_website.pl
@@ -8,18 +8,17 @@ use warnings;
 
 use TweetPics::Config;
 use TweetPics::Updater;
-use TweetPics::Persistence::Website;
+use TweetPics::Persistence::WebsitePages;
 use TweetPics::Persistence::WebsiteIndexes;
 use TweetPics::Source::LocalDisk;
 
-my $website_persistence = TweetPics::Persistence::Website->new("$FindBin::Bin/../www/", '/');
+my $page_persistence = TweetPics::Persistence::WebsitePages->new("$FindBin::Bin/../www/", '/');
+my $index_persistence = TweetPics::Persistence::WebsiteIndexes->new($page_persistence);
 my $source = TweetPics::Source::LocalDisk->new("$FindBin::Bin/../var/persistence");
-my $updater = TweetPics::Updater->new($source, $website_persistence);
+
+my $updater = TweetPics::Updater->new($source, $page_persistence);
 $updater->update_missing_posts;
 
-
-my $index_persistence = TweetPics::Persistence::WebsiteIndexes->new($website_persistence);
-$source = TweetPics::Source::LocalDisk->new("$FindBin::Bin/../var/persistence");
 $updater = TweetPics::Updater->new($source, $index_persistence);
 $updater->update_all_posts;
 
