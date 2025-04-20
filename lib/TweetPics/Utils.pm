eca0f8e2228182447d4e93f364313d0934513758
Adam Field <adam@adamfield.net>
2025-04-20 12:54:44 -0700

    utf8 fix

--- lib/TweetPics/Utils.pm
+++ lib/TweetPics/Utils.pm
@@ -63,7 +63,7 @@ sub write_to_file
 	my $dir = dirname($filepath); #lazy directory creation
 	make_path($dir) unless -d $dir;
 
-	open my $fh, ">", $filepath or die "Couldn't open $filepath for writing\n";
+	open my $fh, ">:encoding(UTF-8)", $filepath or die "Couldn't open $filepath for writing\n";
 	print $fh $data;
 	close $fh;
 }
